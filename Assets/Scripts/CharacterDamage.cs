29007bd06c15f8e4c8edd1a282a1d4a205d95270
Josh Schonstal <josh@schonstal.org>
2012-05-27 16:07:16 -0700

    I'm sleepy

--- Assets/Scripts/CharacterDamage.cs
+++ Assets/Scripts/CharacterDamage.cs
@@ -5,6 +5,10 @@ public class CharacterDamage : MonoBehaviour {
   public float effectDuration = 1f;
   public float noiseIntensity = 2.5f;
   public float vignetteIntensity = 4f;
+  public float motionBlurIntensity = 0.8f;
+  public Color hurtColor;
+  public float hurtAlpha = 0.3f;
+  public GameObject hurtPlane;
 
   public float lowpassMinimumFrequency = 500f;
   public float lowpassMaximumFrequency = 6000f;
@@ -13,6 +17,7 @@ public class CharacterDamage : MonoBehaviour {
   NoiseAndGrain noise;
   Vignetting vignetting;
   AudioLowPassFilter lowPass;
+  MotionBlur motionBlur;
 
 	void Start () {
 	}
@@ -27,6 +32,9 @@ public class CharacterDamage : MonoBehaviour {
     }
     if(noise != null) noise.strength = noiseIntensity * timer/effectDuration;
     if(vignetting != null) vignetting.intensity = vignetteIntensity * timer/effectDuration;
+    if(motionBlur != null) motionBlur.blurAmount = motionBlurIntensity * timer/effectDuration;
+    hurtColor.a = timer/effectDuration * hurtAlpha;
+    hurtPlane.renderer.material.SetColor("_TintColor", hurtColor); 
 	}
 
   void OnCollisionEnter(Collision other) {
@@ -35,6 +43,7 @@ public class CharacterDamage : MonoBehaviour {
       noise = Camera.main.GetComponent<NoiseAndGrain>() as NoiseAndGrain;
       vignetting = Camera.main.GetComponent<Vignetting>() as Vignetting;
       lowPass = Camera.main.GetComponent<AudioLowPassFilter>() as AudioLowPassFilter;
+      motionBlur = Camera.main.GetComponent<MotionBlur>() as MotionBlur;
       timer = effectDuration;
     }
    }
