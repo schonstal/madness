6b7f5e6d16cc83a8aad11190a092dd21df9bd857
Josh Schonstal <josh@schonstal.org>
2012-06-09 14:44:43 -0700

    Misc crap (unity is making it hard to keep track of changes because it makes huge unreadable diffs)

--- Assets/Scripts/GunBob.cs
+++ Assets/Scripts/GunBob.cs
@@ -3,31 +3,36 @@ using System.Collections;
 
 public class GunBob : MonoBehaviour {
   public Vector2 magnitude = new Vector2(.003f, .001f);
+  public float cameraBobMagnitude = 0.001f;
   public float bobsPerSecond = 2f;
   public GameObject target;
   public float minRunSpeed = 1000f;
   public float returnToNeutralTime = 0.5f;
 
   Vector3 originalPosition = new Vector3(0f, 0f, 0f);
+  Vector3 cameraOriginalPosition = new Vector3(0f, 0f, 0f);
   float bobTimer = 0f;
-  float neutralTimer = 0f;
+
+  Movement playerMovement;
 
 	void Start () {
 	  originalPosition = transform.localPosition;
+    cameraOriginalPosition = transform.parent.localPosition;
+    playerMovement = target.GetComponent<Movement>();
 	}
 	
 	void Update () {
-    if(Mathf.Abs(target.rigidbody.velocity.magnitude) > minRunSpeed) {
-      neutralTimer = 0;
-      bobTimer += Time.deltaTime * 2 * Mathf.PI * bobsPerSecond;
-      transform.localPosition = new Vector3(
-          originalPosition.x + magnitude.x * Mathf.Sin(bobTimer),
-          originalPosition.y - magnitude.y * Mathf.Abs(Mathf.Cos(bobTimer)),
-          originalPosition.z);
-    } else {
-      bobTimer = 0;
-      neutralTimer += Time.deltaTime / returnToNeutralTime;
-      transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, neutralTimer);
-    }
+    bobTimer += Time.deltaTime * 2 * Mathf.PI * bobsPerSecond * 
+        (target.rigidbody.velocity.magnitude/playerMovement.maxForward);
+
+    transform.parent.localPosition = new Vector3(
+        cameraOriginalPosition.x,
+        cameraOriginalPosition.y - cameraBobMagnitude * Mathf.Sin(bobTimer*2),
+        cameraOriginalPosition.z);
+
+    transform.localPosition = new Vector3(
+        originalPosition.x + magnitude.x * Mathf.Sin(bobTimer),
+        originalPosition.y - magnitude.y * Mathf.Abs(Mathf.Cos(bobTimer)),
+        originalPosition.z);
 	}
 }
