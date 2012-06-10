5444749a980e590ab31781675093dff68fb22184
Josh Schonstal <josh@schonstal.org>
2012-06-09 21:35:02 -0700

    Secret doors are added randomly

--- Assets/Scripts/Levels/SecretDoor.cs
+++ Assets/Scripts/Levels/SecretDoor.cs
@@ -2,7 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class SecretDoor : MonoBehaviour {
+  public float probability = 0.2f;
+
   void OnDrawGizmos() {
     Gizmos.DrawIcon(transform.position + new Vector3(0f,2.5f,0f), "EditorIcons/Secret.png", true);
   }
+
+	void Start () {
+    if(Random.value < probability) {
+      transform.Find("WallTile").gameObject.SetActiveRecursively(false);
+      transform.Find("SecretDoor").gameObject.SetActiveRecursively(true);
+    } else {
+      transform.Find("WallTile").gameObject.SetActiveRecursively(true);
+      transform.Find("SecretDoor").gameObject.SetActiveRecursively(false);
+    }
+	}
 }
